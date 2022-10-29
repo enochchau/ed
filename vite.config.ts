ed903cd0da7ac90e63011bb04b98d016efb8be4f
ec965 <22567131+ec965@users.noreply.github.com>
2022-10-29 01:58:34 -0700

    Remove vite react plugin
    Remove vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   define: {
     process: {}
   },
-  plugins: [solidPlugin(), react()]
+  plugins: [solidPlugin()]
 })

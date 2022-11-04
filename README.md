ecf97627805c327b31997de8ea410816ef7857c1
ec965 <22567131+ec965@users.noreply.github.com>
2022-11-03 21:42:11 -0700

    Add license and update README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# Prose Mirror Solid Node Views
+
+There's some example code in this repo on how to use [ProseMirror](https://prosemirror.net/) 
+Node views with [SolidJS](https://www.solidjs.com/).
+I was eventually going to turn this into a ProseMirror framework for Solid and I still might
+but I haven't had the time to flesh it out yet at the moment.
+I'm opening up this repo in case anyone wants to see this example implementation.
+
+---
+
 # Ed
 
 working name: Ice
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import "./styles.scss";
 import "prosemirror-view/style/prosemirror.css";
 import applyDevTools from "prosemirror-dev-tools";
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal, onMount } from "solid-js";
 import { DevtoolsOverlay } from "@solid-devtools/overlay";
 import { render } from "solid-js/web";
 import { EditorContent } from "./EditorContent";
 import { keymapPlugin } from "./plugins/keymap";
-import { Editor } from "./editor";
+import { Editor, EditorConstructorParams } from "./editor";
 import { historyPlugin } from "./plugins/history";
 import { docPlugin } from "./plugins/doc";
 import { paragraphPlugin } from "./plugins/paragraph";
@@ -17,29 +17,38 @@ import { dividerPlugin } from "./plugins/divider";
 let appRoot = document.getElementById("app")! as HTMLDivElement;
 
 const App: Component = () => {
+  let editorRef: HTMLDivElement;
+  let [editor, setEditor] = createSignal<null | Editor>();
+
+  onMount(() => {
+    let ed = new Editor({
+      plugins: [
+        keymapPlugin,
+        historyPlugin,
+        docPlugin,
+        paragraphPlugin,
+        textPlugin,
+        headingPlugin,
+        dividerPlugin,
+      ],
+      content: (schema) => schema.node("doc", null, [schema.node("paragraph")]),
+      bindingElement: editorRef,
+    });
+    setEditor(ed);
+  });
+
+  createEffect(() => {
+    let ed = editor();
+    if (ed) {
+      applyDevTools(ed.view);
+    }
+  });
+
   return (
     <EditorContent
       class="ed-page"
-      ref={(editorViewEl) => {
-        let editor = new Editor({
-          plugins: [
-            keymapPlugin,
-            historyPlugin,
-            docPlugin,
-            paragraphPlugin,
-            textPlugin,
-            headingPlugin,
-            dividerPlugin,
-          ],
-          content: (schema) =>
-            schema.node("doc", null, [schema.node("paragraph")]),
-          bindingElement: editorViewEl,
-        });
-
-        if (import.meta.env.DEV) {
-          applyDevTools(editor.view);
-        }
-      }}
+      // @ts-ignore
+      ref={editorRef}
     />
   );
 };
